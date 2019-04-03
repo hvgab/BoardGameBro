18e6e7b16ac641b4324cf26ab27fa81010955d81
Gabbeh <henrik.v.gabrielsen@gmail.com>
2019-04-03 03:29:18 +0200

    Basic API

--- boardgamebro/boardgamebro/settings.py
+++ boardgamebro/boardgamebro/settings.py
@@ -37,7 +37,15 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'social_django',
     'bgb_app',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES':
+    ['rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly']
+}
 
 INTERNAL_IPS = os.getenv('INTERNAL_IPS', '127.0.0.1')
 
